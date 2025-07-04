3e62aeb0078fdf275c02c01f47a617d99e9de012
Aditya Tripathi <aditya@climactic.co>
2025-07-04 23:20:07 +0000

    fix: https on deployment + worker s6 + back to dockerfile

--- apps/web/bootstrap/app.php
+++ apps/web/bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\TrustProxies;
 use App\Http\Middleware\WorkspaceMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -23,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
             WorkspaceMiddleware::class,
+            TrustProxies::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
